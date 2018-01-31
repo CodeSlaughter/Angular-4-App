f9972b728368c20b0ed4b2c8c90a6d4b76fa825a
Iam Slaughter <jlgslaughter@gmail.com>
2018-01-30 22:48:03 -0500

    assignment 2

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,13 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = "No server was created!!";
+  serverName = '';
+  serverCreated = false;
+  username = '';
+  userAdded = false;
+
   constructor() { 
     setTimeout(()=> {
       this.allowNewServer = true;
     }, 2000);
   }
-  
+
   ngOnInit() {
+  }
+
+  onCreateServer(){
+    this.serverCreated = true;
+    this.serverCreationStatus = "Server was created! Name is" + this.serverName;
+  }
+  onUpdateServerName(event: Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+  onAddUser(){
+    this.userAdded = true;
+    this.username = '';
+  }
+  userEmpty(){
+    if(this.username === ''){
+      return true
+    }
+    return false
   }
 
 }
